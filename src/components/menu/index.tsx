35960a91811234989cfd70b71f85bfbf4f818005
Nur Nisrina Ningrum <nisrina.ningrum@shopee.com>
2024-01-02 18:55:52 +0700

    feat: add header with menu

---
+++ src/components/menu/index.tsx
@@ -0,0 +1,75 @@
+import Link from "next/link";
+import { useRouter } from "next/router";
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Button } from "@/components/ui/button";
+import { RxHamburgerMenu } from "react-icons/rx";
+
+const Menu = () => {
+  const router = useRouter();
+  const isActive = (path: string) => router.pathname === path;
+
+  return (
+    <>
+      <div className="md:hidden">
+        <DropdownMenu>
+          <DropdownMenuTrigger className="pt-2">
+            <RxHamburgerMenu size={20} />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="mr-7">
+            <DropdownMenuItem className="w-36">
+              <Link href="/">Home</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Link href="/products">Our Products</Link>
+            </DropdownMenuItem>
+            <DropdownMenuItem>
+              <Link href="/telemedicines">Telemedicine</Link>
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>
+              <Link href="/auth/login" className="font-semibold">
+                Login
+              </Link>
+            </DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      </div>
+      <div className="hidden md:flex flex-row gap-10 items-center">
+        <ul className="flex flex-row gap-5 ">
+          <li
+            className={`text-sm font-normal ${
+              isActive("/") ? "text-[#5CCCE5]" : "hover:text-[#5CACE5]"
+            }`}
+          >
+            <Link href="/">Home</Link>
+          </li>
+          <li
+            className={`text-sm font-normal ${
+              isActive("/products") ? "text-[#5CCCE5]" : "hover:text-[#5CACE5]"
+            }`}
+          >
+            <Link href="/products">Our Products</Link>
+          </li>
+          <li
+            className={`text-sm font-normal ${
+              isActive("/telemedicines")
+                ? "text-[#5CCCE5]"
+                : "hover:text-[#5CACE5]"
+            }`}
+          >
+            <Link href="/telemedicines">Telemedicine</Link>
+          </li>
+        </ul>
+        <Button className="px-10">Login</Button>
+      </div>
+    </>
+  );
+};
+export default Menu;
