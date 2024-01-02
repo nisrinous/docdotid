2bd92379a619061baedb1e3f9584c0c5c87f8d3d
Nur Nisrina Ningrum <nisrina.ningrum@shopee.com>
2024-01-02 19:30:02 +0700

    feat: add login page

---
+++ src/components/form/login-form.tsx
@@ -0,0 +1,86 @@
+import React from "react";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+
+import { LoginSchema } from "@/lib/validation/auth";
+import { loginUser } from "@/lib/db";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { PasswordInput } from "../input/password";
+
+type Inputs = z.infer<typeof LoginSchema>;
+
+const LoginForm = () => {
+  const [isSending, setIsSending] = React.useState(false);
+
+  const form = useForm<Inputs>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  async function onSubmit(formData: Inputs) {
+    try {
+      setIsSending(true);
+      await loginUser(formData.email, formData.password);
+    } catch (error) {
+      console.error("Error registering user:", error);
+    } finally {
+      setIsSending(false);
+    }
+  }
+
+  return (
+    <>
+      <Form {...form}>
+        <form
+          className="grid gap-4"
+          onSubmit={(...args) => void form.handleSubmit(onSubmit)(...args)}
+        >
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>E-mail</FormLabel>
+                <FormControl>
+                  <Input type="text" placeholder="E-mail" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Password</FormLabel>
+                <FormControl>
+                  <PasswordInput placeholder="Password" {...field} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button type="submit" disabled={isSending} className="my-5 w-full">
+            Submit
+          </Button>
+        </form>
+      </Form>
+    </>
+  );
+};
+
+export default LoginForm;
