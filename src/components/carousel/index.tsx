e56041af4e8e874133ec3a6e13abd3a38cc0df08
Nur Nisrina Ningrum <nisrina.ningrum@shopee.com>
2024-01-03 18:47:14 +0700

    feat: add product categories on dashboard

--- src/components/carousel/index.tsx
+++ src/components/carousel/index.tsx
@@ -9,14 +9,6 @@ import {
 } from "@/components/ui/carousel";
 
 const PromotionsCarousel = () => {
-  const promos: string[] = [
-    "Retail markdown-pana.svg",
-    "Combo offer-pana.svg",
-    "Discount-pana.svg",
-    "12.12 Sale-pana.svg",
-    "Pop-up-pana.svg",
-  ];
-
   return (
     <div className="container my-20 flex flex-col justify-center items-center">
       <h3 className="scroll-m-20 text-3xl tracking-tight text-center mt-5">
@@ -31,13 +23,16 @@ const PromotionsCarousel = () => {
           className="w-full mt-7 mb-10"
         >
           <CarouselContent>
-            {promos.map((value, index) => (
+            {Array.from({ length: 6 }).map((_, index) => (
               <CarouselItem key={index} className="md:basis-1/2 lg:basis-1/3">
                 <div className="p-1">
                   <Card>
                     <Link href="/" key={index}>
                       <CardContent className="p-0 flex flex-row h-40 md:h-44 items-center justify-center">
-                        <img src={value} className="h-full"></img>
+                        <img
+                          src={`promo${index + 1}.svg`}
+                          className="h-full"
+                        ></img>
                       </CardContent>
                     </Link>
                   </Card>
---
+++ src/components/categories/product-categories.tsx
@@ -0,0 +1,40 @@
+import Link from "next/link";
+import { Card, CardContent, CardFooter } from "../ui/card";
+
+const ProductCategories = () => {
+  return (
+    <>
+      <div className="container my-10 flex flex-col justify-center border-b-[1px] pb-5">
+        <h3 className="scroll-m-20 text-2xl md:text-3xl tracking-tight text-left mt-5">
+          Shop Wellness Essentials
+        </h3>
+        <p className="text-zinc-400 mb-5">Browse our products by categories</p>
+        <div className="grid grid-cols-3 md:grid-cols-6 lg:grid-cols-10 gap-4">
+          {Array.from({ length: 10 }).map((_, index) => (
+            <Card
+              key={index}
+              className="p-2 border-none shadow-none flex flex-col justify-between"
+            >
+              <CardContent className="p-1 flex flex-col items-center justify-center">
+                <img
+                  src={`promo` + (index + 1) + `.svg`}
+                  className="h-full rounded-full border-[1px]"
+                ></img>
+              </CardContent>
+              <CardFooter className="items-center justify-center p-0">
+                <Link
+                  href="/telemedicines/cardiologist"
+                  className="hover:underline"
+                >
+                  <p className="px-2 text-center">Cardiologist</p>
+                </Link>
+              </CardFooter>
+            </Card>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default ProductCategories;
