e2494dd4865320d0a8894afbdb2c3f062812744b
Nur Nisrina Ningrum <nisrina.ningrum@shopee.com>
2024-01-03 19:32:01 +0700

    fix: add background on header

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,7 +3,7 @@ import Menu from "../menu";
 
 const Header = () => {
   return (
-    <header className="sticky top-0 z-50 w-full border-b bg-background">
+    <header className="bg-white sticky top-0 z-50 w-full border-b bg-background">
       <div className="container flex h-16 items-center justify-between">
         <Logo />
         <Menu />
