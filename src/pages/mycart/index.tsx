4f01e868eb9d5caad357ffb92aeb82d1a25c06f5
Nur Nisrina Ningrum <nisrina.ningrum@shopee.com>
2024-01-05 19:17:32 +0700

    fix: destination on checkout button

--- src/pages/mycart/index.tsx
+++ src/pages/mycart/index.tsx
@@ -2,7 +2,7 @@ import CartItemCard from "@/components/card/cart-item-card";
 import DeliveryCourierCard from "@/components/card/delivery-courier-card";
 import { Button } from "@/components/ui/button";
 import { Card, CardHeader } from "@/components/ui/card";
-import { Checkbox } from "@/components/ui/checkbox";
+// import { Checkbox } from "@/components/ui/checkbox";
 import Link from "next/link";
 
 export default function MyCart() {
@@ -12,12 +12,12 @@ export default function MyCart() {
         <div className="col-span-3 md:col-span-2 flex flex-col">
           <div className="flex flex-row justify-between gap-4 border-b-2 pb-5">
             <h3 className="scroll-m-20 text-2xl md:text-3xl">My Cart</h3>
-            <div className="flex items-center space-x-2 ">
+            {/* <div className="flex items-center space-x-2 ">
               <Checkbox id="terms" />
               <label className="text-sm leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
                 Select All Items
               </label>
-            </div>
+            </div> */}
           </div>
           <div className="py-5 flex flex-col gap-3">
             <Card className="px-5 w-full flex flex-col justify-start items-start gap-1">
@@ -43,7 +43,7 @@ export default function MyCart() {
             Summary
           </h3>
           <div className="flex flex-col gap-3">
-            <div className="flex flex-row justify-between">
+            <div className="flex flex-row justify-between mt-2">
               <p className="text-zinc-600 leading-none text-sm">Order total</p>
               <p className="text-zinc-600 leading-none text-sm">Rp total</p>
             </div>
@@ -61,10 +61,10 @@ export default function MyCart() {
               <p className="text-zinc-600 leading-none text-base">
                 Total price
               </p>
-              <p className="text-zinc-600 leading-none text-base">Rp total</p>
+              <p className="leading-none text-base">Rp total</p>
             </div>
           </div>
-          <Link href="/products">
+          <Link href="/checkout">
             <Button className="w-full my-5 h-12 px-0 mx-0">Checkout</Button>
           </Link>
         </div>
