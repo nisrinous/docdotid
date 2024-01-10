37577b9bfbbf889b1a91daeb70507d18cbdf3e36
ameliejuliette <150439462+ameliejuliette@users.noreply.github.com>
2024-01-10 17:05:54 +0700

    feat: add dashboard ui for superadmin page

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -3,8 +3,21 @@ import React from "react";
 
 const Admin = () => {
   return (
-    <div>
+    <div className="flex">
       <AsideBar />
+      <div className="w-full mx-10 mt-5">
+        <h1 className="text-black text-2xl my-2 mb-8">Dashboard</h1>
+        <div className="w-full flex gap-10">
+          <div className="w-1/2 bg-blue-200 rounded-lg h-60">Hello</div>
+          <div className="w-1/2 bg-red-200 rounded-lg h-60">Hello</div>
+        </div>
+
+        <div className="mt-8">
+          <h2 className="text-xl">Earnings by Pharmacy</h2>
+
+          <table></table>
+        </div>
+      </div>
     </div>
   );
 };
