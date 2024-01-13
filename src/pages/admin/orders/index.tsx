0cdd9fb90a507c1c6fe31c980097389673080344
ameliejuliette <150439462+ameliejuliette@users.noreply.github.com>
2024-01-13 13:40:43 +0700

    feat : update ui for admin orders page

--- src/pages/admin/orders/index.tsx
+++ src/pages/admin/orders/index.tsx
@@ -1,6 +1,46 @@
-import useSWR from "swr";
-import axios from "axios";
-import { MdEmail } from "react-icons/md";
+"use client";
+
+import * as React from "react";
+import { ProductCategoriesResponse } from "@/types";
+import { useSelector } from "react-redux";
+import { RootState } from "@/store/store";
+import { getProducts } from "@/lib/fetcher/products";
+import { Label } from "@/components/ui/label";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog"
+import {
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
+import { ArrowUpDown, ChevronDown, MoreHorizontal } from "lucide-react";
+import AsideBar from "@/components/aside-bar";
+import { Button } from "@/components/ui/button";
+import { Checkbox } from "@/components/ui/checkbox";
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
 import {
   Table,
   TableBody,
@@ -9,90 +49,253 @@ import {
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import Dropdown from "@/components/filter-dropdown";
-import AsideBar from "@/components/aside-bar";
-import SearchBar from "@/components/search-bar";
-import { Button } from "@/components/ui/button";
-import { IoLogoWhatsapp } from "react-icons/io";
+import { useEffect, useState } from "react";
 
-import { useState } from "react";
+export const columns: ColumnDef<ProductCategoriesResponse[]>[] = [
+  {
+    id: "select",
+    header: ({ table }) => (
+      <Checkbox
+        // checked={
+        //   table.getIsAllPageRowsSelected() ||
+        //   (table.getIsSomePageRowsSelected() && "indeterminate")
+        // }
+        onCheckedChange={(value) => table.toggleAllPageRowsSelected(!!value)}
+        aria-label="Select all"
+      />
+    ),
+    cell: ({ row }) => (
+      <Checkbox
+        checked={row.getIsSelected()}
+        onCheckedChange={(value) => row.toggleSelected(!!value)}
+        aria-label="Select row"
+      />
+    ),
+    enableSorting: false,
+    enableHiding: false,
+  },
+  {
+    accessorKey: "id",
+    header: "ID",
+    cell: ({ row }) => <div className="capitalize">{row.getValue("id")}</div>,
+  },
+  {
+    accessorKey: "name",
+    header: ({ column }) => {
+      return (
+        <Button
+          variant="ghost"
+          onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+        >
+          Name
+          <ArrowUpDown className="ml-2 h-4 w-4" />
+        </Button>
+      );
+    },
+    cell: ({ row }) => <div className="lowercase">{row.getValue("name")}</div>,
+  },
+  {
+    id: "actions",
+    enableHiding: false,
+    cell: ({ row }) => {
+      const payment = row.original;
 
-interface Category {
-  id: number;
-  name: string;
-}
+      return (
+        <DropdownMenu>
+          <DropdownMenuTrigger asChild>
+            <Button variant="ghost" className="h-8 w-8 p-0">
+              <span className="sr-only">Open menu</span>
+              <MoreHorizontal className="h-4 w-4" />
+            </Button>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            <DropdownMenuLabel>Actions</DropdownMenuLabel>
+            <DropdownMenuItem
+            // onClick={() => navigator.clipboard.writeText(prod.id)}
+            >
+              Copy payment ID
+            </DropdownMenuItem>
+            <DropdownMenuSeparator />
+            <DropdownMenuItem>View customer</DropdownMenuItem>
+            <DropdownMenuItem>View payment details</DropdownMenuItem>
+          </DropdownMenuContent>
+        </DropdownMenu>
+      );
+    },
+  },
+] as ColumnDef<ProductCategoriesResponse[]>[];
 
-const fetcher = async (url: string) => {
-  const response = await axios.get(url);
-  return response.data;
-};
-const handleSearch = async (url: string) => {};
-
-const Orders = () => {
-  const [selectedStatus, setSelectedStatus] = useState<string | null>();
-  const { data: categories, error } = useSWR<Category[]>(
-    "/api/categories",
-    fetcher
+export default function OrderPage() {
+  const { token } = useSelector((state: RootState) => state.user);
+  const [productsData, setProductsData] = useState<ProductCategoriesResponse[]>(
+    []
   );
-  const handleStatusChange = (newStatus: string) => {
-    setSelectedStatus(newStatus);
+
+  const fetcher = async () => {
+    try {
+      const data = await getProducts(token);
+      setProductsData(data.data);
+    } catch (error) {
+      console.error("" + error);
+    }
   };
 
+  useEffect(() => {
+    fetcher();
+    console.log(productsData);
+  });
+
+  console.log("productsDATA",productsData);
+
+  const [sorting, setSorting] = React.useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = React.useState<ColumnFiltersState>(
+    []
+  );
+  const [columnVisibility, setColumnVisibility] =
+    React.useState<VisibilityState>({});
+  const [rowSelection, setRowSelection] = React.useState({});
+
+  const table = useReactTable({
+    productsData,
+    columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection,
+    },
+  });
+
   return (
-    <div className="flex flex-row">
+    <div className="flex">
       <AsideBar />
       <div className="w-full mx-10 mt-5">
-        <div className="flex justify-between">
-          <h1 className="text-black text-2xl mt-2">Manage Orders</h1>
-          <div className="flex gap-5">
-            <Dropdown
-              buttonLabel={selectedStatus ?? "Edit Status"}
-              menuItems={["Pending", "On Progress", "Completed"]}
-              defaultSelected={selectedStatus}
-              onSelect={(newStatus) => handleStatusChange(newStatus)}
-            />
-            <SearchBar
-              id="table-search"
-              placeholder="Search for addresses by city..."
-              onChange={handleSearch}
-            />
+        <h1 className="text-black text-3xl mt-2 font-bold">
+          Manage Orders
+        </h1>
+        <div className="flex items-center justify-between py-4">
+          <Input
+            placeholder="Filter orders by pharmacy..."
+            value={(table.getColumn("name")?.getFilterValue() as string) ?? ""}
+            onChange={(event) =>
+              table.getColumn("email")?.setFilterValue(event.target.value)
+            }
+            className="max-w-sm"
+          />
+          <div className="flex gap-3">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="outline" className="ml-auto">
+                Columns <ChevronDown className="ml-2 h-4 w-4" />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align="end">
+              {table
+                .getAllColumns()
+                .filter((column) => column.getCanHide())
+                .map((column) => {
+                  return (
+                    <DropdownMenuCheckboxItem
+                      key={column.id}
+                      className="capitalize"
+                      checked={column.getIsVisible()}
+                      onCheckedChange={(value) =>
+                        column.toggleVisibility(!!value)
+                      }
+                    >
+                      {column.id}
+                    </DropdownMenuCheckboxItem>
+                  );
+                })}
+            </DropdownMenuContent>
+          </DropdownMenu>
           </div>
         </div>
-
-        <Table>
-          <TableHeader>
-            <TableRow>
-              <TableHead>ID</TableHead>
-              <TableHead>Name</TableHead>
-              <TableHead>Status</TableHead>
-              <TableHead>Actions</TableHead>
-            </TableRow>
-          </TableHeader>
-          <TableBody>
-            <TableRow>
-              <TableCell>ID</TableCell>
-              <TableCell>Name</TableCell>
-              <TableCell>Status</TableCell>
-              <TableCell className="flex gap-3">
-                <Button className="">
-                  <MdEmail />
-                </Button>
-                <Button className="bg-green-500 hover:bg-green-400">
-                  <IoLogoWhatsapp />
-                </Button>
-              </TableCell>
-            </TableRow>
-            {/* {categories.map((category) => (
-              <TableRow key={category.id}>
-                <TableCell>{category.id}</TableCell>
-                <TableCell>{category.name}</TableCell>
-              </TableRow>
-            ))} */}
-          </TableBody>
-        </Table>
+        <div className="rounded-md border">
+          <Table>
+            <TableHeader>
+              {table.getHeaderGroups().map((headerGroup) => (
+                <TableRow key={headerGroup.id}>
+                  {headerGroup.headers.map((header) => {
+                    return (
+                      <TableHead key={header.id}>
+                        {header.isPlaceholder
+                          ? null
+                          : flexRender(
+                              header.column.columnDef.header,
+                              header.getContext()
+                            )}
+                      </TableHead>
+                    );
+                  })}
+                </TableRow>
+              ))}
+            </TableHeader>
+            <TableBody>
+              {/* {table.getRowModel().rows?.length ? (
+                table.getRowModel().rows.map((row) => (
+                  <TableRow
+                    key={row.id}
+                    data-state={row.getIsSelected() && "selected"}
+                  >
+                    {row.getVisibleCells().map((cell) => (
+                      <TableCell key={cell.id}>
+                        {flexRender(
+                          cell.column.columnDef.cell,
+                          cell.getContext()
+                        )}
+                      </TableCell>
+                    ))}
+                  </TableRow>
+                ))
+              ) : ( */}
+                <TableRow>
+                  <TableCell
+                    colSpan={columns.length}
+                    className="h-24 text-center"
+                  >
+                    No results.
+                  </TableCell>
+                </TableRow>
+              )}
+            </TableBody>
+          </Table>
+        </div>
+        <div className="flex items-center justify-end space-x-2 py-4">
+          <div className="flex-1 text-sm text-muted-foreground">
+            {/* {table.getFilteredSelectedRowModel().rows.length} of{" "}
+            {table.getFilteredRowModel().rows.length} row(s) selected. */}
+          </div>
+          <div className="space-x-2">
+            <Button
+              variant="outline"
+              size="sm"
+              onClick={() => table.previousPage()}
+              disabled={!table.getCanPreviousPage()}
+              className="bg-sky-200"
+            >
+              Previous
+            </Button>
+            <Button
+              variant="outline"
+              size="sm"
+              // onClick={() => table.nextPage()}
+              // disabled={!table.getCanNextPage()}
+              className="bg-sky-200"
+            >
+              Next
+            </Button>
+          </div>
+        </div>
       </div>
     </div>
   );
-};
-
-export default Orders;
+}
