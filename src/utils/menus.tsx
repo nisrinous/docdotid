3096ba51d64f91e57c92032baed70f5c0480897e
amelie.wibowo <amelie.wibowo@shopee.com>
2024-01-17 19:05:32 +0700

    feat : add menus for pharmacy admin

--- src/utils/menus.tsx
+++ src/utils/menus.tsx
@@ -40,10 +40,47 @@ export const menus = [
     icon: RiHospitalFill,
     margin: true,
   },
+  { name: "Dashboard", link: "/pharmacyadm", icon: BiSolidDashboard },
+  {
+    name: "Manage Pharmacy",
+    link: "/pharmacyadm/pharmacy",
+    icon: RiHospitalFill,
+    margin: true,
+  },
+  {
+    name: "Manage Product Categories",
+    link: "/pharmacyadm/categories",
+    icon: FaBriefcaseMedical,
+    margin: true,
+  },
+  {
+    name: "Manage Products",
+    link: "/pharmacyadm/products",
+    icon: GiMedicines,
+    margin: true,
+  },
   {
     name: "Manage Product Inventory",
-    link: "/admin/inventory",
+    link: "/pharmacyadm/inventory",
     icon: RiHospitalFill,
+    margin: true,
+  },
+  {
+    name: "Manage Stock Mutation",
+    link: "/pharmacyadm/stock",
+    icon: RiHospitalFill,
+    margin: true,
+  },
+  {
+    name: "Manage Orders",
+    link: "/pharmacyadm/orders",
+    icon: FaFilePrescription,
+    margin: true,
+  },
+  {
+    name: "Log Out",
+    link: "/pharmacyadm/pharmacies",
+    icon: IoLogOut,
     margin: true,
   },
   {
