912e26bf82e1f27cc6ed46a52bd87aa47bd656fc
fernando <fernando@shopee.com>
2024-01-17 12:43:26 +0700

    feat: fix view and add product

--- src/pages/admin/products/add/index.tsx
+++ src/pages/admin/products/add/index.tsx
@@ -6,56 +6,137 @@ import { menus } from "@/utils/menus";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { Button } from "@/components/ui/button";
+import { apiBaseUrl } from "@/config";
+import { useSelector } from "react-redux";
+import { RootState } from "@/store/store";
 
 interface DropdownOption {
-  Label: string;
-  value: string;
+  id: number;
+  name: string;
 }
 
 const AddProduct = () => {
+  const url = apiBaseUrl;
+  const { token } = useSelector((state: RootState) => state.user);
+
+  const [manufacturerDropdown, setManufacturerDropdown] = useState<
+    DropdownOption[] | null
+  >(null);
+  const [drugClassificationDropdown, setDrugClassificationDropdown] = useState<
+    DropdownOption[] | null
+  >(null);
+  const [drugFormDropdown, setDrugFormDropdown] = useState<
+    DropdownOption[] | null
+  >(null);
+  const [productCategoryDropdown, setProductCategoryDropdown] = useState<
+    DropdownOption[] | null
+  >(null);
+
+  const fetcher = async (url: string) => {
+    const response = await axios.get(url, {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    });
+    return response.data;
+  };
+
+  const { data: manufacturerOptions, error: manufacturerError } = useSWR(
+    `${url}/manufacturers`,
+    fetcher
+  );
+  const { data: drugClassificationOptions, error: drugClassificationError } =
+    useSWR(`${url}/classifications`, fetcher);
+  const { data: drugFormOptions, error: drugFormError } = useSWR(
+    `${url}/forms`,
+    fetcher
+  );
+  const { data: productCategoryOptions, error: productCategoryError } = useSWR(
+    `${url}/categories`,
+    fetcher
+  );
+
+  useEffect(() => {
+    if (manufacturerOptions) {
+      setManufacturerDropdown(manufacturerOptions.data);
+    }
+  }, [manufacturerOptions]);
+
+  useEffect(() => {
+    if (drugClassificationOptions) {
+      setDrugClassificationDropdown(drugClassificationOptions.data);
+    }
+  }, [drugClassificationOptions]);
+
+  useEffect(() => {
+    if (drugFormOptions) {
+      setDrugFormDropdown(drugFormOptions.data);
+    }
+  }, [drugFormOptions]);
+
+  useEffect(() => {
+    if (productCategoryOptions) {
+      setProductCategoryDropdown(productCategoryOptions.data);
+    }
+  }, [productCategoryOptions]);
+
   const [formData, setFormData] = useState({
     name: "",
     generic_name: "",
     content: "",
-    manufacturer: "",
+    manufacturer_id: 0,
     description: "",
-    drug_classification: "",
-    drug_form: "",
-    product_category: "",
+    drug_classification_id: 0,
+    drug_form_id: 0,
+    product_category_id: 0,
     unit_in_pack: "",
     selling_unit: 0,
     weight: 0,
     height: 0,
     length: 0,
     width: 0,
+    image: "",
   });
 
-  const { data: manufacturerOptions, error: manufacturerError } = useSWR<
-    DropdownOption[]
-  >("www.dropdown.com", async (url: string) => {
-    const response = await axios.get(url);
-    return response.data;
-  });
+  const [img, setImg] = useState<string>("");
+  const [isUploading, setIsUploading] = useState<boolean>(false);
 
-  const { data: drugClassificationOptions, error: drugClassificationError } =
-    useSWR<DropdownOption[]>("www.dropdown1.com", async (url: string) => {
-      const response = await axios.get(url);
-      return response.data;
-    });
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const files = e.target.files;
+    if (!files) return;
 
-  const { data: drugFormOptions, error: drugFormError } = useSWR<
-    DropdownOption[]
-  >("www.dropdown2.com", async (url: string) => {
-    const response = await axios.get(url);
-    return response.data;
-  });
+    const file = files[0];
+    if (!file) return;
 
-  const { data: productCategoryOptions, error: productCategoryError } = useSWR<
-    DropdownOption[]
-  >("www.dropdown3.com", async (url: string) => {
-    const response = await axios.get(url);
-    return response.data;
-  });
+    uploadToCloudinary(file);
+  };
+
+  const uploadToCloudinary = async (file: File) => {
+    setIsUploading(true);
+
+    const fileData = new FormData();
+    fileData.append("file", file);
+    fileData.append("upload_preset", "docdotid");
+
+    try {
+      const response = await fetch(
+        "https://api.cloudinary.com/v1_1/minevf/image/upload",
+        {
+          method: "POST",
+          body: fileData,
+        }
+      );
+
+      if (response.ok) {
+        const data = await response.json();
+        setImg(data.secure_url);
+      }
+    } catch (error) {
+      console.error("Error uploading to Cloudinary:", error);
+    }
+
+    setIsUploading(false);
+  };
 
   const handleInputChange = (e: { target: { name: any; value: any } }) => {
     const { name, value } = e.target;
@@ -65,8 +146,33 @@ const AddProduct = () => {
   const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
 
+    if (typeof formData.manufacturer_id === "string") {
+      formData.manufacturer_id = parseInt(formData.manufacturer_id, 10);
+    }
+    if (typeof formData.drug_classification_id === "string") {
+      formData.drug_classification_id = parseInt(
+        formData.drug_classification_id,
+        10
+      );
+    }
+    if (typeof formData.drug_form_id === "string") {
+      formData.drug_form_id = parseInt(formData.drug_form_id, 10);
+    }
+    if (typeof formData.product_category_id === "string") {
+      formData.product_category_id = parseInt(formData.product_category_id, 10);
+    }
+
+    formData.image = img;
+
+    console.log(formData);
+
     try {
-      const response = await axios.post("www.backend.com", formData);
+      const response = await axios.post(`${url}/products`, formData, {
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+      });
       console.log("Response:", response.data);
     } catch (error) {
       console.error("Error submitting form:", error);
@@ -79,6 +185,18 @@ const AddProduct = () => {
       <div className="w-full mx-10 mt-5">
         <h1 className="text-black text-3xl mt-2 font-bold">Manage Products</h1>
         <form onSubmit={handleSubmit}>
+          <div>
+            <Label htmlFor="picture">Picture:</Label>
+            <Input
+              id="picture"
+              type="file"
+              onChange={handleImageChange}
+              accept="image/*"
+            />
+            {isUploading && <p>Uploading image...</p>}
+            {img !== "" && <img src={img} alt="Product" />}
+          </div>
+
           <div>
             <Label htmlFor="name">Name:</Label>
             <Input
@@ -119,17 +237,15 @@ const AddProduct = () => {
             <Label htmlFor="manufacturer">Manufacturer:</Label>
             <select
               id="manufacturer"
-              name="manufacturer"
-              value={formData.manufacturer}
+              name="manufacturer_id"
+              value={formData.manufacturer_id}
               onChange={handleInputChange}
               required
             >
-              <option value="" disabled>
-                Select Manufacturer
-              </option>
-              {manufacturerOptions?.map((option) => (
-                <option key={option.value} value={option.value}>
-                  {option.Label}
+              <option value="0">Select Manufacturer</option>
+              {manufacturerDropdown?.map((option) => (
+                <option key={option.id} value={option.id}>
+                  {option.name}
                 </option>
               ))}
             </select>
@@ -151,17 +267,15 @@ const AddProduct = () => {
             <Label htmlFor="drug_classification">Drug Classification:</Label>
             <select
               id="drug_classification"
-              name="drug_classification"
-              value={formData.drug_classification}
+              name="drug_classification_id"
+              value={formData.drug_classification_id}
               onChange={handleInputChange}
               required
             >
-              <option value="" disabled>
-                Select Drug Classification
-              </option>
-              {drugClassificationOptions?.map((option) => (
-                <option key={option.value} value={option.value}>
-                  {option.Label}
+              <option value="0">Select Drug Classification</option>
+              {drugClassificationDropdown?.map((option) => (
+                <option key={option.id} value={option.id}>
+                  {option.name}
                 </option>
               ))}
             </select>
@@ -171,17 +285,15 @@ const AddProduct = () => {
             <Label htmlFor="drug_form">Drug Form:</Label>
             <select
               id="drug_form"
-              name="drug_form"
-              value={formData.drug_form}
+              name="drug_form_id"
+              value={formData.drug_form_id}
               onChange={handleInputChange}
               required
             >
-              <option value="" disabled>
-                Select Drug Form
-              </option>
-              {drugFormOptions?.map((option) => (
-                <option key={option.value} value={option.value}>
-                  {option.Label}
+              <option value="0">Select Drug Form</option>
+              {drugFormDropdown?.map((option) => (
+                <option key={option.id} value={option.id}>
+                  {option.name}
                 </option>
               ))}
             </select>
@@ -191,17 +303,15 @@ const AddProduct = () => {
             <Label htmlFor="product_category">Product Category:</Label>
             <select
               id="product_category"
-              name="product_category"
-              value={formData.product_category}
+              name="product_category_id"
+              value={formData.product_category_id}
               onChange={handleInputChange}
               required
             >
-              <option value="" disabled>
-                Select Product Category
-              </option>
-              {productCategoryOptions?.map((option) => (
-                <option key={option.value} value={option.value}>
-                  {option.Label}
+              <option value="0">Select Product Category</option>
+              {productCategoryDropdown?.map((option) => (
+                <option key={option.id} value={option.id}>
+                  {option.name}
                 </option>
               ))}
             </select>
@@ -220,7 +330,7 @@ const AddProduct = () => {
           </div>
 
           <div>
-            <Label htmlFor="selling_unit">Selling Unit:</Label>
+            <Label htmlFor="selling_unit">Price:</Label>
             <Input
               type="number"
               id="selling_unit"
