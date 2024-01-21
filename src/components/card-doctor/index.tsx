74e5a49b8e8d4bad0c845c2e4f3337beb13bb451
Nur Nisrina Ningrum <nisrina.ningrum@shopee.com>
2024-01-21 16:53:38 +0700

    fix: doctor card

--- src/components/card-doctor/index.tsx
+++ src/components/card-doctor/index.tsx
@@ -6,23 +6,31 @@ import { DoctorResponse } from "@/types";
 
 export function CardDoctor({ doctor }: { doctor: Partial<DoctorResponse> }) {
   return (
-    <Card className="w-full md:w-80 flex flex-row justify-evenly items-center">
+    <Card className="w-full md:w-80 flex flex-row justify-center items-center">
       <div className="flex flex-col items-center gap-1">
         <img
           alt="Profile Image"
           className="w-24 items-center"
           src="https://res-console.cloudinary.com/minevf/media_explorer_thumbnails/6cb8f67a33376e09150a0ff78061df2e/detailed"
         />
-        <Badge className="bg-green-700/60">
-          {doctor.is_active ? "Online" : "Offline"}
-        </Badge>
+        {doctor.is_active ? (
+          <Badge className="bg-green-600/60">Online</Badge>
+        ) : (
+          <Badge className=" bg-gray-600/60">Offline</Badge>
+        )}
       </div>
       <CardContent className="p-4">
-        <h2 className="text-2xl font-semibold mb-2">{doctor.user_name}</h2>
+        <h2 className="text-2xl font-semibold mb-2">
+          Dr. {doctor.user_name?.split(" ")[0]}
+        </h2>
         <p className="text-gray-600 mb-0">{doctor.specialist_name}</p>
         <p className="text-lg font-bold mb-4">${doctor.fee}</p>
         <Link href={`/telemedicine/${doctor?.id}`}>
-          <Button className="bottom-4 right-0" variant="outline">
+          <Button
+            disabled={!doctor.is_active}
+            className="bottom-4 right-0"
+            variant="outline"
+          >
             Start Chat
           </Button>
         </Link>
